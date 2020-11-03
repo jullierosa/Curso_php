e5a903aa7985dabe04eee83a219611330c289bbc
jullierosa <julliecdsr@gmail.com>
2020-11-03 15:34:26 -0300

    new excercise

---
+++ exer_aritmeticos/exer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operações Aritméticas</title>
+</head>
+<body>
+    <?php 
+        $n1 = 4;
+        $n2 = 2;
+        echo "A soma vale ". ($n1+$n2);
+        echo "<br>A subtração vale ". ($n1-$n2);
+        echo "<br>A multiplicação vale ". ($n1*$n2);
+        echo "<br>A divisão vale ". ($n1/$n2);
+        echo "<br>O módulo vale ". ($n1%$n2);
+        echo "<br>A média dos 2 valores são ". ($n1+$n2)/2; 
+    ?>
+    
+</body>
+</html>
