22c072922a3f9bdaa0b311fbae58dea22f169ad5
jullierosa <julliecdsr@gmail.com>
2020-11-06 18:21:23 -0300

    Create exerc.php

---
+++ exerc_votos/exerc.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $id = $_GET["id"];
+     $ano = $_GET["a"];
+     echo "Você nasceu em " . ($ano-$id);
+     echo "<br>Você tem $id anos";
+     echo "<br>Voce " . (($id>=18 && $id<65)?"é obrigado a votar":"não é obrigado a votar ");
+    ?>
+</body>
+</html>
