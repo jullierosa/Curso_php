74e07870b3ed6f456d5ead9acdca69b5d0f68fc9
jullierosa <julliecdsr@gmail.com>
2020-11-06 17:17:20 -0300

    Create exerc.php

---
+++ exerc_media_esc/exerc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nome = $_GET["n"];//nome passado pela URL
+    /*As notas também foram passadas pela URL */
+    $n1 = $_GET["x"];
+    $n2 = $_GET["y"];
+    $n3 = $_GET["z"];
+    $m =  ($n1+$n2+$n3)/3;//média calculada
+    echo "<h1>Notas Escolares</h1>";
+    echo "$nome, a média entre $n1, $n2 e $n3 é $m";
+    $res = $m >= 6?"aprovado":"reprovado";
+    echo "<br> E voce está $res";
+    ?>
+</body>
+</html>
