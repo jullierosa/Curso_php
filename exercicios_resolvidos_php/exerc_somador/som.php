2c38bd0b7738b41b5db3e3f10ed46f762e4d3a78
jullierosa <julliecdsr@gmail.com>
2021-02-17 10:42:53 -0300

    new exerc

---
+++ exercicios_resolvidos_php/exerc_somador/som.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>somador</title>
+</head>
+<body>
+    <?php 
+     $n1 = 3;//declaração de var
+     $n2 = 5;//declaração de var
+     $soma = $n1 + $n2; //soma
+     echo "A soma de $n1 + $n2 é <b> $soma </b>"; //<b></b> deixa em NEGRITO
+    ?>
+</body>
+</html>
