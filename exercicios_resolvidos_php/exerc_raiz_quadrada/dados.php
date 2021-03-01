d4c806641fcf4691fafee11c4f98073545d40e54
jullierosa <julliecdsr@gmail.com>
2021-03-01 10:11:20 -0300

    commit

--- exercicios_resolvidos_php/exerc_raiz_quadrada/dados.php
+++ exercicios_resolvidos_php/exerc_raiz_quadrada/dados.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Raiz Quadrada</title>
+    <title>Document</title>
 </head>
 <body>
-    <form method="get" action="dados.php">
-        valor: <input type="number" name="v">
-        <input type="submit" value="Calcular Raiz">
-    </form>
+    
 </body>
 </html>
