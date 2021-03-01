89b345c163f55686e60a75dbbbd0ef0b0418fa9e
jullierosa <julliecdsr@gmail.com>
2021-03-01 11:32:29 -0300

    commit

--- exercicios_resolvidos_php/formularios/02dados.php
+++ exercicios_resolvidos_php/formularios/02dados.php
@@ -12,7 +12,7 @@
         $ano = $_GET["ano"];
         $sexo = $_GET["sexo"];
         $idade = date("Y") - $ano;
-        echo "<b> $nome, "
+        echo "<b> $nome, é do sexo $sexo"
     ?>
 </body>
 </html>
